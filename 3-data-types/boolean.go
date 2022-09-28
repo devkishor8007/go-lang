8f777a43095f2fc89d4a3ac0169341052811ccd6
Kishor KC <kishor@esignature.com.np>
2022-09-28 09:33:43 +0545

    add data types

---
+++ 3-data-types/boolean.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	isAge := 10 > 1
+	fmt.Println(isAge)
+}
+
+
+// greater than
+// 250 > 200 true
+// 20 > 50 false
+
+// less than
+// 200 < 400 true
+// 4 < 2 false
+
+// equal
+// 51 = 51 true
+// 500 = 280 false
---
+++ 3-data-types/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var pi float64 = 3.14
+	pi= 2.25
+
+	var apple int = 270
+	discount := 70
+	apple = apple - discount
+	fmt.Println("the value of PI is ", pi)
+	fmt.Println("the price of 1 kg apple is", apple)
+}
