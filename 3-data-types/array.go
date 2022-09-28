c6766c35cafe9e7308d726293f23718a4f2a3fa9
Kishor KC <kishor@esignature.com.np>
2022-09-28 10:25:58 +0545

    updated at array

--- 3-data-types/array.go
+++ 3-data-types/array.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	fruit := [3]string{"apple", "banana", "orange"}
 	fmt.Println(fruit);
+
+	marks := [...]int {75, 88, 69, 82}
+	price := [4]int {1500, 750, 2500, 1965}
+	fmt.Println(marks, price)
 }
