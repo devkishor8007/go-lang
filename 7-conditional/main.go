5d2300ba3381613c47f9bc59603ecb94a965399e
Kishor KC <kishor@esignature.com.np>
2022-10-11 17:41:54 +0545

    add switch on conditional

--- 7-conditional/main.go
+++ 7-conditional/main.go
@@ -12,4 +12,15 @@ func main() {
 	} else {
 		fmt.Println("age must be 18 or above 18")
 	}
+
+	// switch case
+
+	switch age {
+	case 15: 
+	    fmt.Println("Not qualified")
+	case 18:
+		fmt.Println("you are in")
+    case 20:
+		fmt.Println("you are under qualified")
+	}
 }
